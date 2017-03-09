272dd0e33ce55c0164a8065d27154a478e47f194
Chan Khim <ckhim@mail.com>
2017-03-09 15:40:14 -0500

    person1 Pluto mod

--- WelcomeJava.java
+++ WelcomeJava.java
@@ -9,7 +9,7 @@ public class WelcomeJava
 		System.out.println( "Hello, world!" );
 		System.out.println( "Hello, world!" );
 		System.out.println( "Hello, world!" );
-		System.out.println( "Hello, world!" );
+		System.out.println( "Hello, Pluto!" );
 
 	}
 }
